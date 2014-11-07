4961cd50c0c7073b82cf6ce03fafa3c679ede745
MaiHo <maingocho94@gmail.com>
2014-11-07 00:31:04 -0800

    Added files.

---
+++ chirps/parse_keys.py
@@ -0,0 +1,4 @@
+# For accessing the Parse backend
+PARSE_HOSTNAME = 'https://api.parse.com'
+PARSE_APP_ID = 'uJkZ5zjVVRwaB1iaXzlxyw94ujbg9wFxIswXLUlu'
+PARSE_REST_API_KEY = 'XawzpRCPUbQEdaPJ4O471u7xJKmnqk0IGLdNCdRD'
